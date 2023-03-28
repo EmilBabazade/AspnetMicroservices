cd071b774c395fdbcdb63095f579cfebab23d4d5
Emil <emilbabazade29@gmail.com>
2023-03-28 19:52:29 +0300

    testing the microservice

---
+++ microservice-map-info/MicroserviceGRPCTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using microservice_map_info.Protos;
+
+var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7253"));
+var client = new DistanceInfo.DistanceInfoClient(channel);
+var response = await client
+    .GetDistanceAsync(new Cities
+    {
+        OriginCity = "Riga, LV",
+        DestinationCity = "Baku, AZE"
+    });
+Console.WriteLine(response.Miles);
+Console.ReadKey();
